40fb2d93d45d0ad0556731986bc3b4c39ba5ff8f
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-03-19 21:32:25 -0400

    tiem for standup

--- index.js
+++ index.js
@@ -204,8 +204,18 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor{
+constructor(obj1) {
+  super(obj1);
+  this.gradClassName = obj1.gradClassName;
+  this.favInstructor = obj1.favInstructor;
+  }
+  standUp(channel){
+    return `${this.name} announces to ${channel}, @channel sandy times!`;
+  }
+  debugsCode(student, subject) {
+    return `${this.name} debugs ${this.name}'s code on ${subject}`;
+  }
 }
 
 /*
