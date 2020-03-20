21e644b95f3bd8282ad3f18090bd5bd39e123aa2
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-03-19 20:41:43 -0400

    Instructor done

--- index.js
+++ index.js
@@ -143,8 +143,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian{
+  constructor(obj1) {
+    super(obj1);
+    this.specialty = obj1.specialty;
+    this.favLanguage = obj1.favLanguage;
+    this.catchPhrase = obj1.catchPhrase;
+  }
+    demo(subject) {
+      return `Today we are learning about ${subject}`;
+    }
+    grade(student, subject) {
+      return `${student} receives a perfect score on ${subject}`;
+    }
 }
 
 /*
